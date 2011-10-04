d369d5e8413c80eccdeed530195b6d85533463f3
subv <subv@73cf1824-746b-4eb0-ba16-0c255e03c809>
2011-10-04 22:14:54 +0000

    upload image approve
    git-svn-id: svn+ssh://yazoonweb/home/svn/projects/iws@265 73cf1824-746b-4eb0-ba16-0c255e03c809

--- src/huard/iws/bean/PageBodyImageBean.java
+++ src/huard/iws/bean/PageBodyImageBean.java
@@ -6,8 +6,11 @@ import huard.iws.util.*;
 public class PageBodyImageBean {
 	private int id;
 	private String name;
+	private String captionHebrew;
+	private String captionEnglish;
 	private byte [] image;
 	private int uploaderPersonId;
+	private int approved;
 
 
 	private String title;
@@ -27,16 +30,23 @@ public class PageBodyImageBean {
 	public PageBodyImageBean( PageBodyImage pageBodyImage){
 		this.id = pageBodyImage.getId();
 		this.name = pageBodyImage.getName();
+		this.captionHebrew = pageBodyImage.getCaptionHebrew();
+		this.captionEnglish = pageBodyImage.getCaptionEnglish();
 		this.image = pageBodyImage.getImage();
 		this.uploaderPersonId = pageBodyImage.getUploaderPersonId();
+		this.approved = pageBodyImage.getApproved();
 	}
 
 	public PageBodyImage toPageBodyImage(){
 		PageBodyImage pageBodyImage = new PageBodyImage();
 		pageBodyImage.setId(id);
 		pageBodyImage.setName(name);
+		pageBodyImage.setCaptionHebrew(captionHebrew);
+		pageBodyImage.setCaptionEnglish(captionEnglish);
 		pageBodyImage.setImage(image);
 		pageBodyImage.setUploaderPersonId(uploaderPersonId);
+		pageBodyImage.setApproved(approved);
+
 		return pageBodyImage;
 	}
 
@@ -72,11 +82,29 @@ public class PageBodyImageBean {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getCaptionHebrew() {
+		return captionHebrew;
+	}
+	public void setCaptionHebrew(String captionHebrew) {
+		this.captionHebrew = captionHebrew;
+	}
+	public String getCaptionEnglish() {
+		return captionEnglish;
+	}
+	public void setCaptionEnglish(String captionEnglish) {
+		this.captionEnglish = captionEnglish;
+	}
 	public int getUploaderPersonId() {
 		return uploaderPersonId;
 	}
 	public void setUploaderPersonId(int uploaderPersonId) {
 		this.uploaderPersonId = uploaderPersonId;
+	}
+	public int getApproved() {
+		return approved;
+	}
+	public void setApproved(int approved) {
+		this.approved = approved;
 	}
 
 
