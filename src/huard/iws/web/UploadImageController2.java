3fd2e9ee6400fb955ecb870cfcc13941fad5d5e6
subv <subv@73cf1824-746b-4eb0-ba16-0c255e03c809>
2011-10-04 22:18:51 +0000

    git-svn-id: svn+ssh://yazoonweb/home/svn/projects/iws@270 73cf1824-746b-4eb0-ba16-0c255e03c809

--- src/huard/iws/web/UploadImageController2.java
+++ src/huard/iws/web/UploadImageController2.java
@@ -4,21 +4,27 @@ import huard.iws.bean.PageBodyImageBean;
 import huard.iws.bean.PersonBean;
 import huard.iws.util.RequestWrapper;
 import huard.iws.model.PageBodyImage;
+import huard.iws.service.MessageService;
 import huard.iws.service.PageBodyImageService;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.ImageIcon;
+import java.io.*;
 
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.multipart.support.ByteArrayMultipartFileEditor;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 public class UploadImageController2 extends GeneralFormController {
 
@@ -33,11 +39,33 @@ public class UploadImageController2 extends GeneralFormController {
 		else if (action != null && action.equals("delete")) {
 			String id = request.getParameter("imageId", "");
 			pageBodyImageService.deletePageBodyImage(Integer.parseInt(id));
+		}
+		else if (action != null && action.equals("approve")) {
+			String id = request.getParameter("imageId", "");
+			pageBodyImageService.approvePageBodyImage(Integer.parseInt(id));
 		} else {
-			int uploaderId = userPersonBean.getId();
-			pageBodyImageBean.setUploaderPersonId(uploaderId);
-			pageBodyImageService.insertPageBodyImage(pageBodyImageBean
-					.toPageBodyImage());
+			//check size and format
+			int imgwidth = pageBodyImageBean.getWidth();
+			int imgheight = pageBodyImageBean.getHeight();
+			int imgsize =pageBodyImageBean.getImage().length;
+			String extention = "";
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest)request.getRequest();
+			Iterator fileNames = multipartRequest.getFileNames();
+			while (fileNames.hasNext()) {
+				String filename = (String) fileNames.next();
+				MultipartFile file = multipartRequest.getFile(filename);
+				String originalFilename = file.getOriginalFilename();
+				extention = originalFilename.substring(originalFilename.indexOf(".")+1);
+			}
+			if(imgheight==240 && imgwidth==420 && imgsize< 300000 && extention.equalsIgnoreCase("jpg")){
+				int uploaderId = userPersonBean.getId();
+				pageBodyImageBean.setUploaderPersonId(uploaderId);
+				pageBodyImageService.insertPageBodyImage(pageBodyImageBean.toPageBodyImage());
+			}
+		    else{
+				String userMessage = messageService.getMessage("iw_IL.uploadImage.imageNotCorrect");
+				request.getSession().setAttribute("userMessage", userMessage);
+		    }
 		}
 		
 		return new ModelAndView(new RedirectView("uploadImage.html"));
@@ -48,8 +76,6 @@ public class UploadImageController2 extends GeneralFormController {
 			Map<String, Object> model) throws Exception {
 
 		
-		  //PageBodyImageBean command = (PageBodyImageBean) model.get("command");
-		 
 		  int page = 0; 
 		  String aPage; 
 		  if ( request.getParameter("page","") != null && !request.getParameter("page","").equals("")){ 
@@ -94,5 +120,11 @@ public class UploadImageController2 extends GeneralFormController {
 			PageBodyImageService pageBodyImageService) {
 		this.pageBodyImageService = pageBodyImageService;
 	}
+	private MessageService messageService;
+
+	public void setMessageService(MessageService messageService) {
+		this.messageService = messageService;
+	}
+
 
 }
