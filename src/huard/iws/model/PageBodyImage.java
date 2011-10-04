bad49104231deadfa2748b1c108bf19f735ed1f0
subv <subv@73cf1824-746b-4eb0-ba16-0c255e03c809>
2011-10-04 22:16:21 +0000

    upload image approve
    git-svn-id: svn+ssh://yazoonweb/home/svn/projects/iws@268 73cf1824-746b-4eb0-ba16-0c255e03c809

--- src/huard/iws/model/PageBodyImage.java
+++ src/huard/iws/model/PageBodyImage.java
@@ -3,8 +3,11 @@ package huard.iws.model;
 public class PageBodyImage {
 	private int id;
 	private String name;
+	private String captionHebrew;
+	private String captionEnglish;
 	private byte [] image;
 	private int uploaderPersonId;
+	private int approved;
 	
 	public int getId() {
 		return id;
@@ -24,11 +27,29 @@ public class PageBodyImage {
 	public void setName(String name) {
 		this.name = name;
 	}
+	public String getCaptionHebrew() {
+		return captionHebrew;
+	}
+	public void setCaptionHebrew(String captionHebrew) {
+		this.captionHebrew = captionHebrew;
+	}
+	public String getCaptionEnglish() {
+		return captionEnglish;
+	}
+	public void setCaptionEnglish(String captionEnglish) {
+		this.captionEnglish = captionEnglish;
+	}
 	public int getUploaderPersonId() {
 		return uploaderPersonId;
 	}
 	public void setUploaderPersonId(int uploaderPersonId) {
 		this.uploaderPersonId = uploaderPersonId;
+	}
+	public int getApproved() {
+		return approved;
+	}
+	public void setApproved(int approved) {
+		this.approved = approved;
 	}
 
 
