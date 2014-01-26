8f12cae10ccb10a25ce875f990ad51fef04e83ed
subv <subv@73cf1824-746b-4eb0-ba16-0c255e03c809>
2014-01-26 08:54:13 +0000

    text utils include hebrew
    git-svn-id: svn+ssh://yazoonweb/home/svn/projects/iws@1038 73cf1824-746b-4eb0-ba16-0c255e03c809

--- src/huard/iws/util/TextUtils.java
+++ src/huard/iws/util/TextUtils.java
@@ -71,7 +71,7 @@ public class TextUtils {
 	}
 	public static String cleanBrFromEditor(String html){
 		html=html.trim();
-		//html=html.replaceAll("[^\\x00-\\x7F]", "");
+		html=html.replaceAll("[^\\x00-\\x7F,^\\u0590-\\u05FF]", "");
 		html=html.replaceAll("^<br>$","");//replace if only break in field
 		return html;
 	}
